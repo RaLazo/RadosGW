5980e84f0bcd346f08a4bab7c0bf4ef4300a1f88
Lazen <rafaellazenhofer@gmail.com>
2018-02-17 19:24:20 +0100

    aa

--- GUI/graphic.py
+++ GUI/graphic.py
@@ -157,6 +157,7 @@ class gui_branding(QMainWindow,QTabWidget):
             a.append(QCheckBox( parent=self.table ))
             self.table.setCellWidget(i, 0, a[i])
 <<<<<<< HEAD
+<<<<<<< HEAD
             a[i].clicked.connect(lambda: self.checker(a,x))
             self.table.setItem(i, 1, QTableWidgetItem(x[i]))
             self.table.setItem(i, 2, QTableWidgetItem(y))
@@ -193,6 +194,11 @@ class Account_Popup(QWidget):
        
 
     
+=======
+            a[i].clicked.connect(self.checkbox_clicked)
+            self.table.setItem(i, 1, QTableWidgetItem(y[0]))
+            self.table.setItem(i, 2, QTableWidgetItem(y[1]))
+>>>>>>> parent of 3035c31... Adepting Account
 =======
             a[i].clicked.connect(self.checkbox_clicked)
             self.table.setItem(i, 1, QTableWidgetItem(y[0]))
