b82d33a51503a3fb7cd0a5a27dedb95ad4cf17df
Lazen <rafaellazenhofer@gmail.com>
2018-04-04 08:25:18 +0200

    Editing

--- GUI/popup.py
+++ GUI/popup.py
@@ -1,8 +1,20 @@
-# Title: RadosGW - GUI - Popup
-# Eng.: Rafael Lazenhofer
-# Ver.: 1.0
-# Date: 02.03.2018
-#
+'''
+  RadosGW - GUI - Popup adaption to the RadosGW - Connector
+    Copyright (C) 2017  Rafael Lazenhofer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
 # Ist für das Accountpopup zuständig
 import sys
 from PyQt5.QtWidgets import *
